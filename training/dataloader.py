572340273ea7ace47c0266f30760f2102c83290c
Nirmal <nirmal@iastate.edu>
2025-04-08 16:28:17 -0500

    both vae and fp working

--- training/dataloader.py
+++ training/dataloader.py
@@ -4,25 +4,20 @@ from torch.utils.data import Dataset, DataLoader
 import h5py
 
 
-attributes = ['ABS_f_D', 'CT_f_D_tort1', 'CT_f_A_tort1']
-
 class H5Dataset(Dataset):
-    def __init__(self, file_path, transform=None):
+    def __init__(self, file_path, attributes, transform=None):
         super(H5Dataset, self).__init__()
         self.file_path = file_path
+        self.attributes = attributes
         with h5py.File(file_path, 'r') as h5_file:
             self.keys = list(h5_file.keys())
         self.transform = transform
 
     def __getitem__(self, index):
         with h5py.File(self.file_path, 'r') as h5_file:
-            # Load image data
-            image = torch.from_numpy(h5_file[self.keys[index]][:].reshape(1,64,64,64)).float()
-
-            # Load labels (the 2nd and 3rd attributes)
+            image = torch.from_numpy(h5_file[self.keys[index]][:].reshape(1, 64, 64, 64)).float()
             attrs = h5_file[self.keys[index]].attrs
-            #label = torch.tensor([attrs['CT_f_A_tort1'], attrs['CT_f_D_tort1']], dtype=torch.float)
-            label = torch.tensor([attrs[attr] for attr in attributes], dtype=torch.float)
+            label = torch.tensor([attrs[attr] for attr in self.attributes], dtype=torch.float)
 
         if self.transform:
             image = self.transform(image)
@@ -33,16 +28,16 @@ class H5Dataset(Dataset):
         return len(self.keys)
 
 class ImageDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size, data_path, transform=None):
+    def __init__(self, batch_size, data_path, attributes, transform=None):
         super().__init__()
         self.batch_size = batch_size
         self.data_path = data_path
+        self.attributes = attributes
         self.transform = transform
 
     def setup(self, stage=None):
-        # Assign train/val datasets for use in dataloaders
         if stage == 'fit' or stage is None:
-            full_dataset = H5Dataset(self.data_path, transform=self.transform)
+            full_dataset = H5Dataset(self.data_path, attributes=self.attributes, transform=self.transform)
             train_size = int(0.8 * len(full_dataset))
             val_size = len(full_dataset) - train_size
             self.train_dataset, self.val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
@@ -52,6 +47,3 @@ class ImageDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
-
-
-
--- training/training.py
+++ training/training.py
@@ -10,6 +10,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from models.VAE import VAE
+from models.FP import SimpleFC
+from models.DDPM import LatentDDPM as DDPM
+
 from dataloader import ImageDataModule
 
 print("module loaded")
@@ -21,7 +24,8 @@ with open("config.yaml", "r") as f:
 # Auto-generate task name if not provided
 if config.get('task') is None:
     time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-    config['task'] = f"VAE_{time}"
+    config['task'] = 'tmp'
+    # config['task'] = f"{time}"
 
 data_path = config['data_path']
 model_dir = config['model_dir']
@@ -30,18 +34,25 @@ if not os.path.exists(model_dir):
 transform = None
 
 print('Loading data')
-data = ImageDataModule(config['batch_size'], data_path, transform=transform)
+data = ImageDataModule(
+    batch_size=config['batch_size'],
+    data_path=data_path,
+    attributes=config['attributes'],
+    transform=transform
+)
+
 data.setup()
 train_loader = data.train_dataloader()
 val_loader = data.val_dataloader()
 print('Data loaded')
 
+# training vae
 # Logging and Callbacks
 wandb_logger = None  # Optional: configure later if needed
 checkpoint_callback = ModelCheckpoint(
     monitor='val_recon_loss',
     dirpath=model_dir,
-    filename=config['task'],
+    filename=f"{config['task']}_vae",
     save_top_k=2,
     mode='min',
 )
@@ -49,20 +60,53 @@ checkpoint_callback = ModelCheckpoint(
 print("Configuration:")
 print(config)
 
-model = VAE(
+vae = VAE(
     latent_dim=config['latent_dim'],
     T_max=config['max_epochs'],
     kld_loss_weight=float(config['kld_loss_weight'])
 )
 
 print('Training')
-trainer = pl.Trainer(
+trainer_vae = pl.Trainer(
     max_epochs=config['max_epochs'],
     logger=wandb_logger,
     callbacks=[checkpoint_callback]
 )
-trainer.fit(model, train_loader, val_loader)
-print('Training complete')
+
+trainer_vae.fit(vae, train_loader, val_loader)
+print('Training vae complete')
 
 # Save the final model
-torch.save(model.state_dict(), os.path.join(model_dir, f"{config['task']}_final_model.pth"))
+torch.save(vae.state_dict(), os.path.join(model_dir, f"vae_{config['task']}_final_model.pth"))
+
+#################################################################################################
+# training FP
+checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',
+    dirpath=model_dir,
+    filename=f"{config['task']}_fp",
+    save_top_k=2,
+    mode='min',
+)
+
+num_features = len(config['attributes'])
+fp = SimpleFC(
+    output_size=num_features,
+    vae = vae,
+    T_max = config['max_epochs'],
+    dropout = 0.1
+)
+
+print('Training FP')
+
+trainer_fp = pl.Trainer(max_epochs=config['max_epochs'],
+                        logger=wandb_logger,
+                        callbacks=[checkpoint_callback])
+trainer_fp.fit(fp, train_loader, val_loader)
+
+print('Training FP complete')
+# Save the final model
+torch.save(fp.state_dict(), os.path.join(model_dir, f"fp_{config['task']}_final_model.pth"))
+
+
+
