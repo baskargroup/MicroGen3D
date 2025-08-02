b322e50d4013c8f2a33aafd8cae0a3e95284a000
Nirmal <nirmal@iastate.edu>
2025-08-02 00:41:08 -0500

    allow multiple pieces of h5 files.

--- training/dataloader.py
+++ training/dataloader.py
@@ -1,25 +1,35 @@
-import pytorch_lightning as pl
+import glob
+import os
+import h5py
 import torch
 from torch.utils.data import Dataset, DataLoader
-import h5py
-
+import pytorch_lightning as pl
 
 class H5Dataset(Dataset):
-    def __init__(self, file_path, attributes, image_shape, transform=None):
-        super(H5Dataset, self).__init__()
-        self.file_path = file_path
+    def __init__(self, file_pattern, attributes, image_shape, transform=None):
+        super().__init__()
+        self.file_paths = sorted(glob.glob(file_pattern))  # List of .h5 files
         self.attributes = attributes
         self.image_shape = image_shape
         self.transform = transform
 
-        with h5py.File(file_path, 'r') as h5_file:
-            self.keys = list(h5_file.keys())
-        self.transform = transform
+        # Collect all (file_index, key) pairs
+        self.index_map = []
+        for file_idx, file_path in enumerate(self.file_paths):
+            with h5py.File(file_path, 'r') as f:
+                keys = list(f.keys())
+                self.index_map.extend([(file_idx, key) for key in keys])
+
+    def __len__(self):
+        return len(self.index_map)
 
     def __getitem__(self, index):
-        with h5py.File(self.file_path, 'r') as h5_file:
-            image = torch.from_numpy(h5_file[self.keys[index]][:].reshape(*self.image_shape)).float()
-            attrs = h5_file[self.keys[index]].attrs
+        file_idx, key = self.index_map[index]
+        file_path = self.file_paths[file_idx]
+
+        with h5py.File(file_path, 'r') as f:
+            image = torch.from_numpy(f[key][:].reshape(*self.image_shape)).float()
+            attrs = f[key].attrs
             label = torch.tensor([attrs[attr] for attr in self.attributes], dtype=torch.float)
 
         if self.transform:
@@ -27,14 +37,11 @@ class H5Dataset(Dataset):
 
         return image, label
 
-    def __len__(self):
-        return len(self.keys)
-
 class ImageDataModule(pl.LightningDataModule):
     def __init__(self, batch_size, data_path, attributes, image_shape, transform=None):
         super().__init__()
         self.batch_size = batch_size
-        self.data_path = data_path
+        self.data_path = data_path  # This can be a glob pattern now
         self.attributes = attributes
         self.image_shape = image_shape
         self.transform = transform
@@ -42,15 +49,17 @@ class ImageDataModule(pl.LightningDataModule):
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
             full_dataset = H5Dataset(
-            file_path=self.data_path,
-            attributes=self.attributes,
-            image_shape=self.image_shape,
-            transform=self.transform
+                file_pattern=self.data_path,
+                attributes=self.attributes,
+                image_shape=self.image_shape,
+                transform=self.transform
             )
-            
+
             train_size = int(0.8 * len(full_dataset))
             val_size = len(full_dataset) - train_size
-            self.train_dataset, self.val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
+            self.train_dataset, self.val_dataset = torch.utils.data.random_split(
+                full_dataset, [train_size, val_size]
+            )
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
